a134539b127fcfb7fce426e3beecf9b85c18b985
Andres Nowak <andresnowak52@gmail.com>
2019-05-01 21:21:54 -0500

    add function save and save_as

--- notes_app_ui.py
+++ notes_app_ui.py
@@ -26,8 +26,28 @@ class Ui_Form(object):
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
+    def setupUiSave(self, Form):
+        self.save = QtWidgets.QPushButton(Form)
+        self.save.setGeometry(QtCore.QRect(0, 20, 75, 23))
+        self.save.setObjectName("save")
+        self.save_as = QtWidgets.QPushButton(Form)
+        self.save_as.setGeometry(QtCore.QRect(0, 40, 75, 23))
+        self.save_as.setObjectName("save_as")
+        self.save_as_text = QtWidgets.QLineEdit(Form)
+        self.save_as_text.setGeometry(QtCore.QRect(80, 40, 201, 21))
+        self.save_as_text.setObjectName("save_as_text")
+
+        self.retranslateUiSave(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUiSave(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.save.setText(_translate("Form", "Save"))
+        self.save_as.setText(_translate("Form", "Save as"))
+
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.pushButton.setText(_translate("Form", "Save"))
+        self.pushButton.setText(_translate("Form", "Options"))
 

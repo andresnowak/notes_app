facf5a5dd35ad6225a8d28ab1bad8e44bde9c73d
Andres Nowak <andresnowak52@gmail.com>
2019-05-12 22:40:15 -0500

    started markdown_to_html_v2

--- markdown_to_html.py
+++ markdown_to_html.py
@@ -15,6 +15,8 @@ class MarkdownToHtml():
         #print(self.contents)
         #self.header_number = 0
         self.text_note = []
+        self.header = False
+        self.header_number = 0
 
         self.note_reader()
 
@@ -25,8 +27,13 @@ class MarkdownToHtml():
             if character == "#":
                 index = self.contents.index(character)
                 self.contents = self.contents[index:]
-                self.header_counter()
+                self.header_checker()
             elif character == "\n":
+                if self.header == True:
+                    index = self.contents.index(character)
+                    self.contents = self.contents[index:]
+                    self.header_checker()
+
                 self.text_note.append("<br>")
             elif character == "*":
                 index = self.contents.index(character)
@@ -41,117 +48,27 @@ class MarkdownToHtml():
         saved_note.close()
         exit()
 
-    def header_counter(self):
-        header_number = 0
+    def header_checker(self):
         index = 0
-        for character in self.contents:
-            if character == "#":
-                header_number += 1
-            else:
-                index = self.contents.index(character)
-                self.contents = self.contents[index:]
-                self.header(self.contents, header_number)
 
-    def header(self, text, header_number):
-        header_text = []
-        index = 0
-        for letter in self.contents:
-            if letter == "\n":
-                header_text = "".join(map(str, header_text))
-                result = "<h{}>{}</h{}>".format(header_number, header_text, header_number)
-                self.text_note.append(result)
-                index = self.contents.index(letter)
-                self.contents = self.contents[index:]
-                self.note_reader()
-
-                #print (result)
-            else:
-                header_text.append(letter)
-
-        header_text = "".join(map(str, header_text))
-        result = "<h{}>{}</h{}>".format(header_number, header_text, header_number)
-        self.contents = ""
-        self.text_note.append(result)
-        self.note_reader()
+        if self.header == True:
+            self.text_note.append("</h{}".format(self.header_number))
+            self.header_number = 0
+            self.header = False
+        else:
+            for character in self.contents:
+                if character == "#":
+                    self.header_number += 1
+                else:
+                    self.text_note.append("<h{}".format(self.header_number))
+                    index = self.contents.index(character)
+                    self.contents = self.contents[index:]
+                    self.header = True
+                    break
+        
+        #TODO: add a header creator here for the special case when the text finishes here
 
     def bold_italic_checker(self):
-        counter = 0
-        index = 0
-        for letter in self.contents:
-            if letter == "*":
-                counter += 1
-            else:
-                index = self.contents.index(letter)
-                if counter == 2:
-                    self.contents = self.contents[index:]
-                    self.bold()
-                elif counter == 1:
-                    self.contents = self.contents[index:]
-                    self.italic()
-                else:
-                    result = self.contents[index - counter:index]
-                    self.text_note.append(result)
-                    self.contents = self.contents[index:]
-    
-    def bold(self):
-        #TODO: change how header and bold work
-        bold_text = []
-        index = 0
-        counter = 0
-        for letter in self.contents:
-            if letter == "*":
-                counter += 1
-            elif counter == 2:
-                bold_text = "".join(map(str, bold_text))
-                result = "<strong>{}</strong>".format(bold_text)
-                self.text_note.append(result)
-                index = self.contents.index(letter)
-                self.contents = self.contents[index:]
-                self.note_reader()
-            elif counter > 2:
-                pass
-                #TODO: add a checker so it doesnt apply the bold if its not equal to two
-            else:
-                bold_text.append(letter)
-        
-        #TODO: add an if here so it checks if the counter is 2 so it doesnt apply the bold if its not equal to 2
-        bold_text = "".join(map(str, bold_text))
-        result = "<strong>{}</strong>".format(bold_text)
-        self.contents = ""
-        self.text_note.append(result)
-        self.note_reader()
-
-    def italic(self):
-        italic_text = []
-        index = 0
-        counter = 0
-        for letter in self.contents:
-            if letter == "*":
-                counter += 1
-            elif counter == 1:
-                italic_text = "".join(map(str, italic_text))
-                result = "<em>{}</em>".format(italic_text)
-                self.text_note.append(result)
-                index = self.contents.index(letter)
-                self.contents = self.contents[index:]
-                self.note_reader()
-            elif counter > 1:
-                #TODO: add a checker so it doesnt apply the bold if its not equal to two
-                pass
-            else:
-                italic_text.append(letter)
-
-        #TODO: add an if here so it checks if the counter is 2 so it doesnt apply the italic if its not equal to 2
-        italic_text = "".join(map(str, italic_text))
-        result = "<em>{}</em>".format(italic_text)
-        self.contents = ""
-        self.text_note.append(result)
-        self.note_reader()
-
-    def paragraph(self, text):
-        return "<p>{text}</p>".format(text=text)
-
-    def create_html(self):
         pass
 
 if __name__ == "__main__":

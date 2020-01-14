db16a0b39507f48137c1227b9da184693b94694f
Andres Nowak <andresnowak52@gmail.com>
2020-01-14 14:45:53 -0600

    Added emphasis checker

--- markdown_to_html.py
+++ markdown_to_html.py
@@ -9,9 +9,10 @@ class MarkdownToHtml():
         self.note_name = note
         note = open(("notes/{}").format(note))
         note = note.read()
-        # NOTE:you can iterate over characters in contents of a read file and  i think strings to
 
         new_note = self.note_reader(note)
+
+        self.bold_syntax = [r"(\*{2}|_{2})", r"\S(\*{2}|_{2})", r"(\*{2}|_{2})\S"]
 
         self.save_note(new_note)
 
@@ -26,10 +27,14 @@ class MarkdownToHtml():
     def note_reader(self, note):
         note = note.split("\n")
         new_note = []
-        #new_string = None
+
+        bold_syntax = [r"(\*{2}|_{2})", r"\S(\*{2}|_{2})", r"(\*{2}|_{2})\S"]
+        italic_syntax = [r"(\*|_)", r"\S(\*|_)", r"(\*|_)\S"]
 
         for line in note:
             line = self.header_checker(line) if self.header_checker(line) != None else line
+            line = self.emphasis_checker(line, bold_syntax, "b")
+            line = self.emphasis_checker(line, italic_syntax, "i")
 
             new_note.append(line)
         
@@ -44,13 +49,32 @@ class MarkdownToHtml():
 
             if header_found != None:
                 header_location = [header_found.span()[0], header_found.span()[1]]
-
-                string = f"<h{index+1}>" + string[header_location[1]::] + f"</h{index+1}>"
+                string = f"<h{index+1}>{string[header_location[1]::]}</h{index+1}>"
 
                 break
         
         return string
 
+    def emphasis_checker(self, string, syntax, html_format):
+        syntax_counter = re.findall(syntax[0], string)
+        syntax_found = False
+
+        for index in range(len(syntax_counter)):
+            if syntax_found:
+                syntax_location = re.search(syntax[1], string).span()
+                string = f"{string[0:syntax_location[0]]}</{html_format}>{string[syntax_location[1]::]}"
+
+                syntax_found = False
+            else:
+                if index != len(syntax_counter) - 1:
+                    syntax_location = re.search(syntax[2], string).span()
+                    string = f"{string[0:syntax_location[0]]}<{html_format}>{string[syntax_location[1]-1::]}"
+
+                    syntax_found = True
+                
+        return string
+
 if __name__ == "__main__":
-    note = input("Write the name of the file you want to convert: ")
+    #note = input("Write the name of the file you want to convert: ")
+    note = "hello3.md"
     MarkdownToHtml(note)
